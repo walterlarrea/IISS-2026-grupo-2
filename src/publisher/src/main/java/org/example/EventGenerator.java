88f8b45f88e1c4023c2b36b143dd64ae01810c32
Walter Larrea <walterlarrea@live.com>
2026-08-27 04:00:54 -0300

    feat: Randomiza el delay entre eventos para darle algo de inconsistencia y realismo

--- src/publisher/src/main/java/org/example/EventGenerator.java
+++ src/publisher/src/main/java/org/example/EventGenerator.java
@@ -12,6 +12,8 @@ public class EventGenerator {
     private final float deltaTemp = 0.3f;
     private final float absoluteMinTemp = -2.0f;
     private final float absoluteMaxTemp = 37.0f;
+
+    private final int delayMillis = 500;
 //    private final Float deltaTemp = 0.3f;
 
     private final Random random = new Random();
@@ -38,16 +40,19 @@ public class EventGenerator {
         float lastTemp = this.startingTemp;
 
         try {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 10; i++) {
                 float min = Math.max(this.absoluteMinTemp, lastTemp - this.deltaTemp);
                 float max = Math.min(this.absoluteMaxTemp, lastTemp + this.deltaTemp);
                 lastTemp = this.random.nextFloat(min, max);
+                // TODO: Podria forzar tendencias sostenidas de elevacion / reduccion de la temperatura para formar cambios mas amplios
 
                 double epochTime = java.time.Instant.now().toEpochMilli() / 1000.0;
                 String message = "{\"id\":" + id + ",\"tC\":" + "%.2f".formatted(lastTemp) + ",\"tF\":0.0,\"ts\":" + "%.3f".formatted(epochTime) + "}";
                 publisher.publish(message);
 
-                TimeUnit.MILLISECONDS.sleep(500);
+                // Randomiza el delay entre eventos para darle algo de inconsistencia e imitar deltas no exactos
+                int delay = this.random.nextInt(delayMillis - 25, delayMillis + 25);
+                TimeUnit.MILLISECONDS.sleep(delay);
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
