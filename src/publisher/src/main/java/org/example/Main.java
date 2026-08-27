f2c870042bd0580c456d11bc24f477c2f0bbfa23
Walter Larrea <walterlarrea@live.com>
2026-08-27 16:44:07 -0300

    feat: manejo de json para no validar y crear el objeto manualemnte

--- src/publisher/src/main/java/org/example/Main.java
+++ src/publisher/src/main/java/org/example/Main.java
@@ -7,13 +7,11 @@ import java.util.concurrent.CountDownLatch;
 public class Main {
     private static EventGenerator eventGenerator;
 
-    static void main() throws InterruptedException {
-        IO.println(String.format("Hello and welcome!"));
-
+    public static void main(String[] args) throws InterruptedException {
         try (BeanScope scope = BeanScope.builder().build()) {
-//            PublisherFactory publisherFactory = scope.get(PublisherFactory.class);
-//
-//            publisherFactory.buildPublisher("home/sensors/temperature");
+            EventGenerator eventGenerator = scope.get(EventGenerator.class);
+
+            eventGenerator.generate();
         } // 3. The scope closes automatically here
 
         new CountDownLatch(1).await();
--- src/publisher/src/main/java/org/example/MyFactory.java
+++ src/publisher/src/main/java/org/example/MyFactory.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
-import jakarta.inject.Singleton;
 
 @Factory
 public class MyFactory {
