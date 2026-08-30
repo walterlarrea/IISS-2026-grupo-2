47dc7be2be139b8e2dad88ff3aee58aafa53e722
ivo <ivoalvira25@gmail.com>
2026-08-30 13:53:53 -0300

    added: build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ENVIRONMENT=${1:-dev}
+
+if [ "$ENVIRONMENT" = "dev" ]; then
+    ENV_FILE="$SCRIPT_DIR/../.env.dev"
+elif [ "$ENVIRONMENT" = "local" ]; then
+    ENV_FILE="$SCRIPT_DIR/../.env.local"
+else
+    echo "Entorno inválido: $ENVIRONMENT"
+    echo "Uso: build.sh dev/local"
+    exit 1
+fi
+
+docker compose --env-file "$ENV_FILE" -f "$SCRIPT_DIR/../docker/docker-compose.yml" build
+echo "Build satisfactorio!"
