f4f844f1f44598575df5e947476b72235bcdc9a7
ivo <ivoalvira25@gmail.com>
2026-09-21 22:03:32 -0300

    added: curls de ejemplo.

---
+++ scripts/curl-ejemplos/rooms-list.sh
@@ -0,0 +1,3 @@
+# Listar todas las habitaciones.
+
+curl -X GET http://localhost:8080/habitaciones -H "X-API-Key: dev-rooms-key"
---
+++ scripts/curl-ejemplos/switch-off.sh
@@ -0,0 +1,8 @@
+# Apagar el switch.
+
+curl -X POST http://localhost:8090/switches \
+  -H "Content-Type: application/json" \
+  -d '{
+    "id": "switch-0",
+    "encendido": false
+  }'
